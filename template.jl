aec2440240fb47239e772faf6be6e5115ddb33de
Sebastian Garrido <sebasgarcep@gmail.com>
2020-02-02 11:53:07 -0500

    Update Julia Template

--- template.jl
+++ template.jl
@@ -5,11 +5,20 @@ Approach:
 using Printf
 
 function main()
+    # Begin time measurement
     start = time()
     result = 0
 
+    # Problem parameters
+
+    # Algorithm parameters
+
+    # Solution
+
+    # Show result
     println(result)
 
+    # End time measurement
     elapsed = time() - start
     @printf("Took: %.4f secs\n", elapsed)
 end
